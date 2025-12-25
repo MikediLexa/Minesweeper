c798d4b420e3513e73fc4d0f6e534a7b3ddfa368
mikedilexa <mi.schneider.90@gmail.com>
2025-12-25 16:20:30 +0100

    1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 25.12.2025
+ *      Author: mike
+ */
+
+#include <iostream>
+#include <ostream>
+
+int main() {
+    std::cout << "HI" << std::endl;
+    return 0;
+}
